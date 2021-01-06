297f3c94d524e504ab65ad51753f2abdd9f23d26
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2021-01-06 12:13:38 -0300

    add some comments

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,6 @@
 # Sudoku solver using backtrack recursion
+
+# Example with an unique solution.
 sudoku = [[5,3,0, 0,7,0, 0,0,0],
           [6,0,0, 1,9,5, 0,0,0],
           [0,9,8, 0,0,0, 0,6,0],
@@ -11,6 +13,7 @@ sudoku = [[5,3,0, 0,7,0, 0,0,0],
           [0,0,0, 4,1,9, 0,0,5],
           [0,0,0, 0,8,0, 0,7,9]]
 
+# Check if a empty cell (x, y) can hold a certain number (value).
 def check(x, y, value, board):
     # Check vertical line
     for i in range(9):
@@ -29,19 +32,23 @@ def check(x, y, value, board):
                 return False
     return True
 
-
+# Find all the possible solutions recursively.
 def solve(board):
+    # We can do some pruning, maybe pass the last cell changed,
+    # so we can iterate starting from it.
     for i in range(9):
         for j in range(9):
-            if(board[i][j] == 0):
-                for k in range(1, 10):
-                    if(check(i, j, k, board)):
+            if(board[i][j] == 0): # if cell is empty
+                for k in range(1, 10): # try every possible value
+                    if(check(i, j, k, board)): # solve it recursively
                         board[i][j] = k
                         solve(board)
                         board[i][j] = 0
                 return
     show(board)
+    input()
 
+# Show the sudoku board.
 def show(board):
     for i in range(9):
         for j in range(9):
@@ -51,5 +58,6 @@ def show(board):
         print('\n', end='')
         if((i+1)%3==0 and i!=8):
             print('\n', end='')
+
 
 solve(sudoku)
